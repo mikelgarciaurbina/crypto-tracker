503a7721824ef28ce517caabab72e6da50af4982
Mikel Garcia <mikel_6@hotmail.com>
2017-11-14 03:40:42 +0100

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = () => {
+  return (
+    <View>
+      <Text>Cryptocurrency App</Text>
+    </View>
+  )
+}
+
+export default Header;
