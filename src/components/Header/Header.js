db14c0484b37078cd8221577ca19f9e2e0377753
Mikel Garcia <mikel_6@hotmail.com>
2017-11-14 03:50:33 +0100

    Refactor Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import styles from './Header.styles';
+
+const Header = () => {
+  return (
+    <View style={styles.headerContainer}>
+      <Text style={styles.header}>Cryptocurrency App</Text>
+    </View>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  headerContainer: {
+    alignItems: 'center',
+    display: 'flex',
+    marginTop: 55,
+  },
+});
