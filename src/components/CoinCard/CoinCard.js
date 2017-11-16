485f08763e7f606e5fb51e998d2d25aaff606610
Mikel Garcia <mikel_6@hotmail.com>
2017-11-16 03:18:24 +0100

    Refactor styles in CoinCard

--- src/components/CoinCard/CoinCard.js
+++ src/components/CoinCard/CoinCard.js
@@ -1,66 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
-import { images } from '../Utils/CoinIcons';
 
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    marginBottom: 20,
-    borderBottomColor: '#e5e5e5',
-    borderBottomWidth: 3,
-    padding: 20,
-  },
-  upperRow: {
-    display: 'flex',
-    flexDirection: 'row',
-    marginBottom: 15,
-  },
-  coinSymbol: {
-    marginTop: 10,
-    marginLeft: 20,
-    marginRight: 5,
-    fontWeight: 'bold',
-  },
-  coinName: {
-    marginTop: 10,
-    marginLeft: 5,
-    marginRight: 20,
-  },
-  seperator: {
-    marginTop: 10,
-  },
-  coinPrice: {
-    marginTop: 10,
-    marginLeft: 'auto',
-    marginRight: 10,
-    fontWeight: 'bold',
-  },
-  image: {
-    width: 35,
-    height: 35,
-  },
-  moneySymbol: {
-    fontWeight: 'bold',
-  },
-  statisticsContainer: {
-    display: 'flex',
-    borderTopColor: '#FAFAFA',
-    borderTopWidth: 2,
-    padding: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-  percentChangePlus: {
-    color: '#00BFA5',
-    fontWeight: 'bold',
-    marginLeft: 5,
-  },
-  percentChangeMinus: {
-    color: '#DD2C00',
-    fontWeight: 'bold',
-    marginLeft: 5,
-  },
-});
+import { images } from '../../Utils/CoinIcons';
+import styles from './CoinCard.styles';
 
 const {
   container,
@@ -86,7 +28,7 @@ const CoinCard = ({
   return (
     <View style={container}>
       <View style={upperRow}>
-        <Image style={styles.image} source={{ uri: images[symbol] }} />
+        <Image style={image} source={{ uri: images[symbol] }} />
         <Text style={coinSymbol}>{symbol}</Text>
         <Text style={seperator}>|</Text>
         <Text style={coinName}>{coin_name}</Text>
