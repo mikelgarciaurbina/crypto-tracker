cf86edc914deea6e6a9928d5385a0f7613643deb
Mikel Garcia <mikel_6@hotmail.com>
2017-11-18 07:09:52 +0100

    Refactor actions

--- src/containers/Crypto.js
+++ src/containers/Crypto.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
 
-import FetchCoinData from '../actions/FetchCoinData';
+import { FetchCoinData } from '../actions';
 import { CoinCard } from '../components';
 
 class Crypto extends Component {
