6d489cba722c68e342173d233d4f54f1bdc8b891
Mikel Garcia <mikel_6@hotmail.com>
2017-11-25 13:01:58 +0100

    Refactor styles

--- src/components/CoinCard/CoinCard.styles.js
+++ src/components/CoinCard/CoinCard.styles.js
@@ -3,31 +3,10 @@ import { StyleSheet } from 'react-native';
 import { THEME } from '../../constants';
 
 export default StyleSheet.create({
-  container: {
-    display: 'flex',
-    marginBottom: 20,
-    borderBottomColor: THEME.GREY,
-    borderBottomWidth: 3,
-    padding: 20,
-  },
-  upperRow: {
-    display: 'flex',
-    flexDirection: 'row',
-    marginBottom: 15,
-  },
-  coinSymbol: {
-    marginTop: 10,
-    marginLeft: 20,
-    marginRight: 5,
-    fontWeight: 'bold',
-  },
   coinNameText: {
     marginTop: 10,
     marginLeft: 5,
     marginRight: 20,
-  },
-  seperator: {
-    marginTop: 10,
   },
   coinPrice: {
     marginTop: 10,
@@ -35,20 +14,25 @@ export default StyleSheet.create({
     marginRight: 10,
     fontWeight: 'bold',
   },
+  coinSymbol: {
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 5,
+    fontWeight: 'bold',
+  },
+  container: {
+    display: 'flex',
+    marginBottom: 20,
+    borderBottomColor: THEME.GREY,
+    borderBottomWidth: 3,
+    padding: 20,
+  },
   image: {
     width: 35,
     height: 35,
   },
   moneySymbol: {
     fontWeight: 'bold',
-  },
-  statisticsContainer: {
-    display: 'flex',
-    borderTopColor: THEME.LIGHT_GREY,
-    borderTopWidth: 2,
-    padding: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
   },
   percentChangePlus: {
     color: THEME.GREEN,
@@ -59,5 +43,21 @@ export default StyleSheet.create({
     color: THEME.RED,
     fontWeight: 'bold',
     marginLeft: 5,
+  },
+  seperator: {
+    marginTop: 10,
+  },
+  statisticsContainer: {
+    display: 'flex',
+    borderTopColor: THEME.LIGHT_GREY,
+    borderTopWidth: 2,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  upperRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginBottom: 15,
   },
 });
