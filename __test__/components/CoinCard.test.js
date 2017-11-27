d28852025538c5d8f352134ddceaea12a8f6c2b1
Mikel Garcia <mikel_6@hotmail.com>
2017-11-27 09:29:51 +0100

    Refactor CoinCard component test

--- __test__/components/CoinCard.test.js
+++ __test__/components/CoinCard.test.js
@@ -5,15 +5,18 @@ import CoinCard from '../../src/components/CoinCard';
 
 test('renders correctly', () => {
   const tree = renderer.create(<CoinCard />).toJSON();
+
   expect(tree).toMatchSnapshot();
 });
 
 test('renders correctly with positive values', () => {
   const tree = renderer.create(<CoinCard symbol="BTC" coinName="Bitcoin" priceUsd="8000" percentChange24h="5" percentChange7d="15" />).toJSON();
+
   expect(tree).toMatchSnapshot();
 });
 
 test('renders correctly with negative values', () => {
   const tree = renderer.create(<CoinCard symbol="BTC" coinName="Bitcoin" priceUsd="8000" percentChange24h="-5" percentChange7d="-15" />).toJSON();
+
   expect(tree).toMatchSnapshot();
 });
